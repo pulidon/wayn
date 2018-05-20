a70775bd5bf0e92919b759189fccda7a03f29355
pulidon <pulidon@outlook.com>
2018-05-19 20:38:23 -0500

    Actualizado Settings.py
    Se agrego la aplicación waynapp

--- wayn/settings.py
+++ wayn/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['wayn.com.co']
 # Application definition
 
 INSTALLED_APPS = [
+    'waynapp.apps.WaynappConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
