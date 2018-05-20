1394ec15334ebc93ae61b8c5229294b0070de53e
pulidon <pulidon@outlook.com>
2018-05-19 20:16:16 -0500

    Actualización views.py
    Se agrego la vista test

--- waynapp/views.py
+++ waynapp/views.py
@@ -4,5 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+# Vista del Home
 def index(request):
 	return render(request, 'waynapp/index.html')
+#Vistal del test
+def test(request):
+	return render(request, 'waynapp/test.html')
