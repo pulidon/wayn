19a5ecb1f77d6d7322ca4d6f97b26c626a008b0d
pulidon <pulidon@outlook.com>
2018-05-19 20:42:03 -0500

    Update settings.py

--- wayn/settings.py
+++ wayn/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['wayn.com.co']
 # Application definition
 
 INSTALLED_APPS = [
-    'waynapp.apps.WaynappConfig'
+    'waynapp.apps.WaynappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
