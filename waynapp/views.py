831b851cff2c6e53e84ade1085f0e5d683abf772
pulidon <pulidon@outlook.com>
2018-05-21 20:23:19 -0500

    Actualización views.py
    Se agregaron las vistas de las otras secciones del sitio.

--- waynapp/views.py
+++ waynapp/views.py
@@ -10,3 +10,27 @@ def index(request):
 #Vistal del test
 def test(request):
 	return render(request, 'waynapp/test.html')
+
+# vista del registro
+def registro(request):
+	return render(request, 'wayn/registro.html')
+
+# vista del plan
+def	plan(request):
+	return render(request, 'wayn/plan.html')
+
+# vista del balancevinos
+def	balancevinos(request):
+	return render(request, 'wayn/balancevinos.html')
+
+# vista del match
+def	match(request):
+	return render(request, 'wayn/match.html')
+
+# vista del checkout
+def	checkout(request):
+	return render(request, 'wayn/checkout.html')
+
+# vista del confirmacion
+def	confirmacion(request):
+	return render(request, 'wayn/confirmacion.html')
