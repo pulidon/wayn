37ca2f31002a12085c4fad450ba0aec0f9b88d98
nico <nicolaspulido89@hotmail.com>
2018-06-11 12:50:33 -0500

    -urls.py modificacion de la url raiz del proyecto -models.py agregados los primeros elementos del modelo de datos

--- waynapp/models.py
+++ waynapp/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+#direccion
+#referenciapayu
+
+class Vinos():
+#cuerpo-cafe
+#astringencia-te
+#frutos_rojos-frutos_rojos
+#frutos_negros-frutos_negros
+#fruta_citrica-fruta_citrica
+#fruta_hueso
+#fruta_tropical
+#aroma
+
+class Subscripciones():
