b81c53d360037fd69937cf49c189291dac9e5f5a
nico <nicolaspulido89@hotmail.com>
2018-06-11 18:31:01 -0500

    -test_tierra.jpg se agrega una imagen para el test. -models.py se modifica el modelo de evaluacion. -admin.py modificacion del panel de administracion.

--- waynapp/admin.py
+++ waynapp/admin.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Usuario, Evaluacion, Vino
+
+# Modificaciones a las vistas del panel de administracion
+class EvaluacionInline(admin.TabularInline):
+	model = Evaluacion
+
+class VinoAdmin(admin.ModelAdmin):
+
+	list_display = (
+		'nombre')
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Evaluacion)
+admin.site.register(Vino)
--- waynapp/models.py
+++ waynapp/models.py
@@ -2,12 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Usuario(models.Model):
+	usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 	evaluacion = models.CharField(max_length=100, null=False)
 #direccion
 #referenciapayu
+
+class Evaluacion(models.Model):
+	usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+	cafe = models.CharField(max_length=100, null=False)
+	frutos = models.CharField(max_length=100, null=False)
+	te = models.CharField(max_length=100, null=False)
+	jugo = models.CharField(max_length=100, null=False)
+	olor = models.CharField(max_length=100, null=False)
+	tierra = models.CharField(max_length=100, null=False)
+	estilo = models.CharField(max_length=100, null=False)
+
 
 class Vino(models.Model):
 	nombre = models.CharField(max_length=100, null=False)
