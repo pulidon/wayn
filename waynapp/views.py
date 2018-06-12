837df6e713b41a8cca4f615fd22ea77b8e4d7eeb
nico <nicolaspulido89@hotmail.com>
2018-06-12 15:39:15 -0500

    actualizacion en la vista de registro e inicio de sesion

--- waynapp/views.py
+++ waynapp/views.py
@@ -2,18 +2,31 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Usuario
 
 # Create your views here.
 # Vista del Home
 def index(request):
 	return render(request, 'waynapp/index.html')
-	
+
 #Vistal del test
 def test(request):
 	return render(request, 'waynapp/test.html')
 
 # vista del registro
 def registro(request):
+	if request.method == 'POST':
+		usuario = User()
+		if usuario.is_valid():
+			usuario.save()
+			return render(request, 'waynapp/match')
+		else:
+			return render(request, 'waynapp/registro.html')
+	elif request.method == 'GET':
+		return render(request, 'waynapp/registro.html')
+	else:
+		return render(request, 'waynapp/registro.html')
 	return render(request, 'waynapp/registro.html')
 
 # vista del plan
