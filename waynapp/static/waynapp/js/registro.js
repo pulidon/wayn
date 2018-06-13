9b81c79a49963df28e0b0164d8e673bdb93619c4
nico <nicolaspulido89@hotmail.com>
2018-06-12 20:21:43 -0500

    actualizado script de registro para ejecutar codigo al cargar la pagina

--- waynapp/static/waynapp/js/registro.js
+++ waynapp/static/waynapp/js/registro.js
@@ -1,75 +1,78 @@
-var localData = JSON.parse(localStorage.getItem('someData'));
-//CUERPO
-if (localData.cafe=='expreso') {
-    document.forms['registro']['cuerpo'].value=100;
-} else if (localData.cafe=='tinto') {
-    document.forms['registro']['cuerpo'].value=80;
-}
-else if (localData.cafe=='latte') {
-    document.forms['registro']['cuerpo'].value=60;
-}
-else if (localData.cafe=='perico') {
-    document.forms['registro']['cuerpo'].value=40;
-}
-else {
-    document.forms['registro']['cuerpo'].value = 20;
-}
-//FRUTOS ROJOS
-if (localData.frutos=='rojos') {
-    fRojos=90;
-    fNegros=20;
-} else if (localData.frutos=='negros') {
-   fRojos=20;
-    fNegros=90;
-}
-else if (localData.frutos=='mixtos') {
-    fRojos=50;
-    fNegros=50;
-}
-//ASTRINGENCIA
-if (localData.te=='negro') {
-    astringencia=90;
-} else if (localData.te=='rojo') {
-   astringencia=50;
-}
-else if (localData.te=='verde') {
-    astringencia=25;
- }
-//FRUTAS
-if (localData.jugo=='citrico-tropical') { //sauvignon blanc
-    citrico=90;
-    fHueso=25;
-    fTropical=90;
-} else if (localData.jugo=='citrico-hueso') {
-   citrico=90;
-    fHueso=90;
-    fTropical=25;
-}
-else if (localData.jugo=='tropical-hueso') {
-    citrico=25;
-    fHueso=90;
-    fTropical=90;
- }
-//AROMA
-if (localData.olor=='hierba') { //sauvignon blanc
-    herbal=80;
-    floral=20;
-    } else if (localData.olor=='floral') {
-    herbal=20;
-    floral=80;
-	} else if (localData.olor=='mixto') {
-		herbal=50;
-		floral=50;
-}
-//TIERRA
-if (localData.tierra=='poco') { //sauvignon blanc
-    tierra=25;
-    } else if (localData.tierra=='medio') {
-    tierra=50;
-  }
-  else if (localData.tierra=='mucho') {
-    tierra=90;
-  }
+window.onload = function(){
+	var localData = JSON.parse(localStorage.getItem('someData'));
+	//CUERPO
+	if (localData.cafe=='expreso') {
+			document.forms['registro']['cuerpo'].value=100;
+	} else if (localData.cafe=='tinto') {
+			document.forms['registro']['cuerpo'].value=80;
+	}
+	else if (localData.cafe=='latte') {
+			document.forms['registro']['cuerpo'].value=60;
+	}
+	else if (localData.cafe=='perico') {
+			document.forms['registro']['cuerpo'].value=40;
+	}
+	else {
+			document.forms['registro']['cuerpo'].value = 20;
+	}
+	//FRUTOS ROJOS
+	if (localData.frutos=='rojos') {
+			fRojos=90;
+			fNegros=20;
+	} else if (localData.frutos=='negros') {
+		 fRojos=20;
+			fNegros=90;
+	}
+	else if (localData.frutos=='mixtos') {
+			fRojos=50;
+			fNegros=50;
+	}
+	//ASTRINGENCIA
+	if (localData.te=='negro') {
+			astringencia=90;
+	} else if (localData.te=='rojo') {
+		 astringencia=50;
+	}
+	else if (localData.te=='verde') {
+			astringencia=25;
+	 }
+	//FRUTAS
+	if (localData.jugo=='citrico-tropical') { //sauvignon blanc
+			citrico=90;
+			fHueso=25;
+			fTropical=90;
+	} else if (localData.jugo=='citrico-hueso') {
+		 citrico=90;
+			fHueso=90;
+			fTropical=25;
+	}
+	else if (localData.jugo=='tropical-hueso') {
+			citrico=25;
+			fHueso=90;
+			fTropical=90;
+	 }
+	//AROMA
+	if (localData.olor=='hierba') { //sauvignon blanc
+			herbal=80;
+			floral=20;
+			} else if (localData.olor=='floral') {
+			herbal=20;
+			floral=80;
+		} else if (localData.olor=='mixto') {
+			herbal=50;
+			floral=50;
+	}
+	//TIERRA
+	if (localData.tierra=='poco') { //sauvignon blanc
+			tierra=25;
+			} else if (localData.tierra=='medio') {
+			tierra=50;
+		}
+		else if (localData.tierra=='mucho') {
+			tierra=90;
+		}
+
+};
 
 
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
