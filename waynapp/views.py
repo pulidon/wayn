f583236a6deb830628ae5523d1e04fac5ce021cc
nico <nicolaspulido89@hotmail.com>
2018-06-12 19:04:40 -0500

    -views.py se incorpora la primera version del algoritmo de comparacion de vinos -match.html se agrega impresion de lista de vinos para pruebas

--- waynapp/views.py
+++ waynapp/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .models import Usuario
+from .models import Usuario, Vino
 
 # Create your views here.
 # Vista del Home
@@ -38,7 +38,41 @@ def	balancevinos(request):
 
 # vista del match
 def	match(request):
-	return render(request, 'waynapp/match.html')
+	lista_vinos = Vino.objects.order_by('pk')
+	test_cuerpo = 60
+	test_frutos_rojos = 90
+	test_frutos_negros = 20
+	test_astringencia = 25
+	test_citrico = 0
+	test_fruta_hueso = 0
+	test_fruta_tropical = 0
+	test_aroma_floral = 50
+	test_aroma_herbal = 50
+	test_tierra = 50
+	lista_puntajes = []
+	for vino in lista_vinos:
+		puntaje = Vino()
+		puntaje.nombre = vino.nombre
+		puntaje.cepa = vino.cepa
+		puntaje.tipo = vino.tipo
+		puntaje.origen = vino.origen
+		puntaje.cuerpo = abs(test_cuerpo - vino.cuerpo)
+		puntaje.frutos_rojos = abs(test_frutos_rojos - vino.frutos_rojos)
+		puntaje.frutos_negros = abs(test_frutos_negros - vino.frutos_negros)
+		puntaje.astringencia = abs(test_astringencia - vino.astringencia)
+		puntaje.citrico = abs(test_citrico - vino.citrico)
+		puntaje.fruta_hueso = abs(test_fruta_hueso - vino.fruta_hueso)
+		puntaje.fruta_tropical = abs(test_fruta_tropical - vino.fruta_tropical)
+		puntaje.aroma_floral = abs(test_aroma_floral - vino.aroma_floral)
+		puntaje.aroma_herbal = abs(test_aroma_herbal - vino.aroma_herbal)
+		puntaje.tierra = abs(test_tierra - vino.tierra)
+		puntaje.total = cuerpo + frutos_rojos + frutos_negros + astringencia + citrico + fruta_hueso + fruta_tropical + aroma_floral + aroma_herbal + tierra
+		lista_puntajes.append(puntaje)
+	lista_puntajes.sort(key=puntaje.total,reverse)
+	context = {
+		'lista_puntajes':lista_puntajes
+	}
+	return render(request, 'waynapp/match.html', context)
 
 # vista del checkout
 def	checkout(request):
