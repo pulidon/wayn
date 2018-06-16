01d5d7299619bb25d6559869fcebd3f53fad5db9
nico <nicolaspulido89@hotmail.com>
2018-06-16 16:13:52 -0500

    agregada funcion redirect

--- waynapp/views.py
+++ waynapp/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from .models import Usuario, Evaluacion, Vino, Plan
