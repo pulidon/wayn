94625d112dc5e2a8bdc2d09083a3d4c4441f3dad
nico <nicolaspulido89@hotmail.com>
2018-06-16 16:22:33 -0500

    correccion en la ubicacion del codigo de almacenamiento de la informacion, para ejecutarse al momento de vambiar el balance de vinos

--- waynapp/static/waynapp/js/radar1.js
+++ waynapp/static/waynapp/js/radar1.js
@@ -154,10 +154,10 @@ $("select").change(function() {
       dataToStore = JSON.stringify(localData);
 	  localStorage.setItem('someData', dataToStore);
     });
-    })
 		var localData = JSON.parse(localStorage.getItem('someData'));
 		document.getElementById("plan").value = localData.plan
 		document.getElementById("balance").value = localData.balance
+    })
   .trigger( "change" );
 });
 
